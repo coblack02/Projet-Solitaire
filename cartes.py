15f478345a720b772defc5136289fd0757bb0ed7
justin <justinzhang75019@gmail.com>
2025-10-11 12:07:36 +0200

    minor fix

--- cartes.py
+++ cartes.py
@@ -2,32 +2,33 @@ from tkinter import Tk, Label
 from PIL import Image, ImageTk
 import random
 
-#cards defini
-family = ('pique', 'trefle', 'carreau', 'coeur')
-height = ('as', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'valet', 'dame', 'roi')
+# cards defini
+family = ("pique", "trefle", "carreau", "coeur")
+height = ("as", "2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi")
 root = Tk()
 
-class cCard():
 
-    def __init__(self,c,h):
+class Card:
+
+    def __init__(self, c, h):
         self.family = c
         self.value = h
-        self.face=False
+        self.face = False
 
-    def afficher_carte(self):
-        randcard = Image.open("cartes/"+self.value+"_"+self.family+".gif")
-        photo=ImageTk.PhotoImage(randcard)
+    def print_card(self):
+        randcard = Image.open("cartes/" + self.value + "_" + self.family + ".gif")
+        photo = ImageTk.PhotoImage(randcard)
         label = Label(root, image=photo)
         label.pack()
 
 
-class cPaquet():
+class Paquet:
 
     def __init__(self):
-        self.deck = [] #the deck is an array of 52 cards (4x13)
+        self.deck = []  # the deck is an array of 52 cards (4x13)
         for c in range(4):
             for h in range(13):
-                new_card = cCard(family[c], height[h]) #height starts at 1
+                new_card = Card(family[c], height[h])  # height starts at 1
                 self.deck.append(new_card)
 
     def Shuffle(self):
@@ -36,50 +37,51 @@ class cPaquet():
             h1, h2 = random.randint(t), random.randint(t)
             self.deck[h1], self.deck[h2] = self.deck[h2], self.deck[h1]
 
-    def draw(self):  #depile
+    def draw(self):  # depile
         t = len(self.deck)
-        res=[]
-        if t>=3:
-            carte1,carte2,carte3 = self.deck[0],self.deck[1],self.deck[2]
-            del(self.deck[0],self.deck[0],self.deck[0]) 
-            return [carte1,carte2,carte3]
-        elif t>0:
-            for i in range (t):
-                carte=self.carte[0]
-                del(self.deck[0])
+        res = []
+        if t >= 3:
+            carte1, carte2, carte3 = self.deck[0], self.deck[1], self.deck[2]
+            del (self.deck[0], self.deck[0], self.deck[0])
+            return [carte1, carte2, carte3]
+        elif t > 0:
+            for i in range(t):
+                carte = self.carte[0]
+                del self.deck[0]
                 res.append(carte)
             return res
         else:
             return None
-        
-    def stack(self,elem):
+
+    def stack(self, elem):
         self.deck.append(elem)
 
-class DiscardPile(cPaquet):
+
+class DiscardPile(Paquet):
     def draw(self):
         t = len(self.deck)
-        if t>0:
-            card=self.deck[0]
-            del(self.deck[0])
+        if t > 0:
+            card = self.deck[0]
+            del self.deck[0]
             return card
         else:
             return None
-        
+
     def top(self):
         return self.deck[0]
-    
+
     def visible(self):
-        t=len(self.deck)
-        res=[]
-        if t>=3:
-            e1,e2,e3=self.deck.draw(),self.deck.draw(),self.deck.draw()
+        t = len(self.deck)
+        res = []
+        if t >= 3:
+            e1, e2, e3 = self.deck.draw(), self.deck.draw(), self.deck.draw()
             self.deck.stack(e3)
             self.deck.stack(e2)
             self.deck.stack(e1)
-            return [e1,e2,e3]
-        elif t>0:
-            for i in range (t):
-                elem=self.deck.draw()
+            return [e1, e2, e3]
+        elif t > 0:
+            for i in range(t):
+                elem = self.deck.draw()
                 self.deck.stack(elem)
                 res.append(elem)
             return res
@@ -87,7 +89,19 @@ class DiscardPile(cPaquet):
             return None
 
 
-class FinalPile(cPaquet):
+class FinalPile(Paquet):
     def __init__(self):
         super().__init__()
-        self.deck = []  #start with an empty deck
+        self.deck = []  # start with an empty deck
+
+    def can_stack(self, elem):
+        if len(self.deck) == 0:
+            return elem.value == "as"
+        else:
+            top_card = self.deck[-1]
+            if top_card.family == elem.family:
+                top_index = height.index(top_card.value)
+                elem_index = height.index(elem.value)
+                return elem_index == top_index + 1
+            else:
+                return False
